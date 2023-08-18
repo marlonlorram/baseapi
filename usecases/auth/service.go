4762a2cc865dd9f84c44451c015a3c78a86573b4
Marlon Lorram <eiskyenet@gmail.com>
2023-08-18 17:16:55 -0300

    refatorado: documentação dos métodos checkPassword e setPassword

--- usecases/auth/service.go
+++ usecases/auth/service.go
@@ -74,7 +74,7 @@ func (s *Service) Register(ctx context.Context, username, password string) (*ent
 	return user, nil
 }
 
-// SetPassword criptografa a senha fornecida e a armazena no objeto usuário.
+// setPassword criptografa a senha fornecida e a armazena no objeto usuário.
 func (s *Service) setPassword(user *entities.User, password string) error {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
@@ -85,7 +85,7 @@ func (s *Service) setPassword(user *entities.User, password string) error {
 	return nil
 }
 
-// CheckPassword compara a senha fornecida com a senha criptografada armazenada no objeto usuário.
+// checkPassword compara a senha fornecida com a senha criptografada armazenada no objeto usuário.
 func (s *Service) checkPassword(user *entities.User, password string) error {
 	err := bcrypt.CompareHashAndPassword(user.Password, []byte(password))
 	if err != nil {
